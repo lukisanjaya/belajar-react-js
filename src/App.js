e0c5f3e206b9c01bcbf2510bdac103650e9ec3cc
Luki Sanjaya <ukykazuki@gmail.com>
2023-07-14 09:30:01 +0700

    Penggunaan State

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Harits",
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Halo Dunia</h1>
+          <h1>Halo {this.state.name}</h1>
         </header>
       </div>
     );
