3d82db29486d8bef9638ff19f1f1dda897a6a606
Luki Sanjaya <ukykazuki@gmail.com>
2023-07-14 09:32:56 +0700

    Penggunaan Props

--- src/App.js
+++ src/App.js
@@ -3,22 +3,21 @@ import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "Harits",
-    };
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Halo {this.state.name}</h1>
+          <Greeting name="Al Hartis" />
         </header>
       </div>
     );
+  }
+}
+
+class Greeting extends Component {
+  render() {
+    return <h1>Halo {this.props.name}</h1>;
   }
 }
 
