397f2e4b9cca479e1ddd963336cfb422fe8a8610
Luki Sanjaya <ukykazuki@gmail.com>
2023-07-14 10:51:48 +0700

    Penggunaan React Router (Routing)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { Component, useState } from "react";
+import React from "react";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Layout from "./Layout";
 
 function App() {
-  const [color, setColor] = useState("BLUE");
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Greeting name="Al Hartis" />
-        Color : {color}
-        <button onClick={() => setColor("RED")}>KLIK</button>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="about" element={<About />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
-}
-
-class Greeting extends Component {
-  render() {
-    return <h1>Halo {this.props.name}</h1>;
-  }
 }
 
 export default App;
